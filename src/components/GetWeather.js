6b0520e23736bf7b415f0c6159bedd02bed93eae
Marcin Godlewski <gh3r@users.noreply.github.com>
2017-10-23 13:18:52 +0200

    Add Open Weather api call and change GetWeather compontent to class

--- src/components/GetWeather.js
+++ src/components/GetWeather.js
@@ -1,16 +1,51 @@
 import React from 'react';
 
-const GetWeather = () => {
-  return (
-    <div className='get-weather'>
-      <input
-        type='text'
-        placeholder='St. George, Utah'
-        className='input'
-      />
-      <button type='button' className='btn'>Get Weather</button>
-    </div>
-  )
-};
+import api from '../utils/api';
+
+class GetWeather extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      city: ''
+    };
+  }
+
+  handleWeatherSearch = () => {
+    api.fetchDailyWeather(this.state.city)
+      .then(res => {
+        console.log(res);
+      })
+  }
+
+  handleInputUpdate = (e) => {
+    const city = e.target.value;
+
+    this.setState({
+        city
+    })
+  }
+
+  render() {
+    return (
+      <div className='get-weather'>
+        <input
+          type='text'
+          placeholder='St. George, Utah'
+          className='input'
+          onChange={this.handleInputUpdate}
+          value={this.state.city}
+        />
+        <button
+          type='button'
+          className='btn'
+          onClick={this.handleWeatherSearch}
+        >
+          Get Weather
+        </button>
+      </div>
+    )
+  };
+}
 
 export default GetWeather;
