60fbdd5e62feee8367cd4f4e0dac7524a564ff40
Marcin Godlewski <gh3r@users.noreply.github.com>
2017-10-25 11:24:42 +0200

    Replace Loading component with animejs based one

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import Anime from 'react-anime';
 
-const Loading = () => {
+const Loading = (props) => {
+  const text = props.text || 'Loading ...'
+  const easingType = props.easing || 'easeOutExpo';
+  const loop = props.loop || true;
+  const letterArray = text.split('');
+
   return (
     <div className='loading-container'>
-      <div className='spinner'>
-        <div className='double-bounce1'></div>
-        <div className='double-bounce2'></div>
-      </div>
+      <Anime
+        opacity={0}
+        duration={1000}
+        easing={easingType}
+        delay={1000}
+        loop={loop}
+      >
+        <Anime
+          scale={[4, 1]}
+          opacity={[0, 1]}
+          translateZ={0}
+          easing={easingType}
+          duration={1300}
+          delay={(el, index) => 70 * index}
+          loop={loop}
+        >
+          {letterArray.map(letter => {
+            return <span className='loading-letter'>{letter}</span>
+          })}
+        </Anime>
+      </Anime>
     </div>
   )
 };
